f0deb242c1313eea10e32c61a73aee2637823de6
dmtetra <123200100+dmtetra@users.noreply.github.com>
2023-01-31 20:22:15 -0500

    Create BannableWords.py
    A list of words that are a bannable offence if message contains

---
+++ BannableWords.py
@@ -0,0 +1,2 @@
+#here is a list of bannable words, something I didn't for obvious reasons didn't write in here. This is just a list to show how it would work
+bannable = ['eggs', 'egg', 'chair', '🤣','other inappropriate words...']
